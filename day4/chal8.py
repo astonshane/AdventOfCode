20054d151550bfd59ea696b31728dd3d0c15616f
Shane Aston <astonshane@gmail.com>
2015-12-08 20:52:09 -0500

    challenge 8

---
+++ day4/chal8.py
@@ -0,0 +1,19 @@
+import md5
+
+secret = "ckczppom"
+
+m = md5.new()
+
+m.update(secret)
+
+i=1;
+while(1):
+    print i,
+    m1 = m.copy()
+    m1.update(str(i))
+    hsh = m1.hexdigest()
+    print hsh
+    if hsh[:6] == "000000":
+        print i
+        break
+    i += 1
