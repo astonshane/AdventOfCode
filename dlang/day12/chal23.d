2a96cd2bfa5d18a4ce0608dfc7520a17ef70c42f
Shane Aston <astonshane@gmail.com>
2015-12-28 17:44:43 -0500

    challenge 23

---
+++ dlang/day12/chal23.d
@@ -0,0 +1,40 @@
+import std.stdio;
+import std.file;
+import std.string;
+import std.conv;
+import std.json;
+
+int count(JSONValue j){
+	if (j.type == JSON_TYPE.INTEGER){
+		return to!int(j.integer);
+	}else if (j.type == JSON_TYPE.ARRAY) {
+		int value = 0;
+		foreach (item; j.array){
+			value += count(item);
+		}
+		return value;
+	}else if (j.type == JSON_TYPE.OBJECT) {
+		int value = 0;
+		foreach (item; j.object){
+			value += count(item);
+		}
+		return value;
+	}
+	return 0;
+}
+
+int main(string[] args){
+	if (args.length != 2){
+		writefln("Needs an input file!");
+		return 1;
+	}
+	string filepath = args[1];
+	foreach (line; File(filepath, "r").byLine){
+		line = line.strip();
+		//writeln("\n", line);
+		JSONValue j = parseJSON(line);
+		writefln("Count: %d", count(j));
+	}
+
+   return 0;
+}
