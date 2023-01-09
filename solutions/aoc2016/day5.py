333501b156f1f7f35acd81d6a6a7be8d6fe7a848
Shane Aston <astonshane@gmail.com>
2023-01-08 19:23:14 -0500

    migrating 2016 day 5

--- solutions/aoc2016/day5.py
+++ solutions/aoc2016/day5.py
@@ -1,40 +1,39 @@
-import md5
+from register import register_solution
+from hashlib import md5
 
-def part1():
+
+@register_solution(2016, 5, 1)
+def part1(filename):
     input = "ojvtpuvg"
     password = ""
     count = 0
 
-    while (len(password) != 8):
-        m = md5.new()
-        m.update(input + str(count))
+    while len(password) != 8:
+        m = md5((input + str(count)).encode())
         digest = m.hexdigest()
 
         if digest[:5] == "00000":
             password += digest[5]
-            print password
+            # print(password)
         count += 1
 
-    print password
+    print(password)
 
-def part2():
+
+@register_solution(2016, 5, 2)
+def part2(filename):
     input = "ojvtpuvg"
-    password = [None]*8
+    password = [None] * 8
     count = 0
 
-    while (None in password):
-        m = md5.new()
-        m.update(input + str(count))
+    while None in password:
+        m = md5((input + str(count)).encode())
         digest = m.hexdigest()
 
         if digest[:5] == "00000":
             if digest[5] in [chr(i) for i in range(ord('0'), ord('8'))] and password[int(digest[5])] is None:
                 password[int(digest[5])] = digest[6]
-                print password
+                # print(password)
         count += 1
 
-
-    print ''.join(password)
-
-#part1()
-part2()
+    print(''.join(password))
